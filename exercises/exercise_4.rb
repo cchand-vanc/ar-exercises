cb5efddd6cdd76fecc8feb2df57bd87f36d1316e
cchand-vanc <cmchandler@live.co.uk>
2023-03-07 16:28:07 -0800

    Finds storesusing where clauses

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,21 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Store.create(name: "Surrey", annual_revenue: "224000", mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: "1900000", mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: "430000", mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+for store in @mens_stores do
+  puts store.name
+  puts store.annual_revenue
+end
+
+@high_revenue = Store.where("annual_revenue < ?", 1000000).where(womens_apparel: true)
+
+for store in @high_revenue do
+  puts store.name
+  puts store.annual_revenue
+end
